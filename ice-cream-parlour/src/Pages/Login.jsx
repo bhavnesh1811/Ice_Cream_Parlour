bc3bef09e7c5331f62b754afbd02aecdea4e595b
bhavnesh1811 <erbhavnesharora@gmail.com>
2023-06-14 23:22:50 +0530

    login route added

---
+++ ice-cream-parlour/src/Pages/Login.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import axios from "axios";
+
+const Login = () => {
+  const nav = useNavigate();
+
+  const handleLogin = async () => {
+    let res = await axios.post(`https://reqres.in/api/login`, {
+      email: "eve.holt@reqres.in",
+      password: "cityslicka",
+    });
+    // console.log(res.data.token);
+    localStorage.setItem("token", JSON.stringify(res.data.token));
+  };
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            to enjoy all of our cool <Link color={"blue.400"}>features</Link> ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Link color={"blue.400"}>Forgot password?</Link>
+              </Stack>
+              <Button
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+                onClick={handleLogin}
+              >
+                Log in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default Login;
--- ice-cream-parlour/src/Routes/AllRoutes.jsx
+++ ice-cream-parlour/src/Routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Customer from "../Pages/Customer";
 import HomePage from "../Pages/HomePage";
+import Login from "../Pages/Login";
 import Owner from "../Pages/Owner";
 
 const AllRoutes = () => {
@@ -10,6 +11,7 @@ const AllRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/owner" element={<Owner />} />
       <Route path="/customer" element={<Customer />} />
+      <Route path="/owner/login" element={<Login />} />
     </Routes>
   );
 };
