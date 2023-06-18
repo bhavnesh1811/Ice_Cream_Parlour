2dd5470d159f719a94aec74629d0a6e3e46966e3
bhavnesh1811 <erbhavnesharora@gmail.com>
2023-06-18 23:47:34 +0530

    all routes loading fixed

--- ice-cream-parlour/src/Pages/Customer/Icecreams.jsx
+++ ice-cream-parlour/src/Pages/Customer/Icecreams.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Icecreams = () => {
-  return (
-    <div>Icecreams</div>
-  )
-}
+  return <div>Icecreams</div>;
+};
 
-export default Icecreams
+export default Icecreams;
